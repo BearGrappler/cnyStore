f188e03969228d26f287a60f139133b7cf36d9b2
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-12 16:14:01 -0500

    completed sequelize product model

---
+++ server/db/models/product.js
@@ -0,0 +1,52 @@
+'use strict';
+var Sequelize = require('sequelize');
+var db = require('../_db');
+
+module.exports = db.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    manufacturer: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    msrp: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        set: function(msrp) {
+            this.setDataValue(msrp * 100)
+        },
+        get: function() {
+            var msrp = this.getDataValue('msrp');
+            return msrp * 100
+        }
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        set: function(price) {
+            this.setDataValue(price * 100)
+        },
+        get: function() {
+            var price = this.getDataValue('price');
+            return price * 100
+        }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    imageUrls: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        allowNull: false
+    },
+    type: {
+        type: Sequelize.ENUM('base', 'ram', 'hdd', 'cpu'),
+        allowNull: false
+    },
+    inventory: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
