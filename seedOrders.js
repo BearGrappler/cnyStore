25ff7a6214cc24bf16d06f64ba6bb02f951aab65
BearGrappler <changingtimes@gmail.com>
2016-09-13 11:29:39 -0500

    Did the seedorders file

--- seedOrders.js
+++ seedOrders.js
@@ -19,65 +19,43 @@ name in the environment files.
 
 var chalk = require('chalk');
 var db = require('./server/db');
-var Product = db.model('order');
+var Order = db.model('order');
 var Promise = require('sequelize').Promise;
 
-var seedProducts = function () {
+var seedOrders = function () {
 
-    var products = [
+    var orders = [
         {
-            name: 'Macbook Silver',
-            manufacturer: 'Apple',
-            msrp: 800.00,
-            price: 999.99,
-            description: 'stylish laptop for college students',
-            imageUrls: ['http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/air/macbook-air-13-select-hero-201505?wid=265&hei=154&fmt=png-alpha&qlt=95&.v=YGXjP3','http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbookair/select/macbookair-select-box-201504?wid=553&hei=399&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=HWal10'
-            ],
-            type: 'base'
+            paymentReceivedDate: '2016-04-02',
+            shippingDate: '2016-04-03',
+            deliveryDate: '2016-04-06',
+            currentStatus: 'delivered'
         },
         {
-            name: 'Alienware Gaming Laptop',
-            manufacturer: 'Dell',
-            msrp: 900.00,
-            price: 1199.99,
-            description: 'gaming device for intense mountain-dew guzzlers',
-            imageUrls: ['http://scene7-cdn.dell.com/is/image/DellComputer/laptop-alienware-15-r2-polaris-mixed-set-video?hei=400&wid=965','http://shop-media.intel.com/api/v2/helperservice/getimage?url=http://images.icecat.biz/img/gallery/29798850_5705.jpg&height=550&width=550'
-            ],
-            type: 'base'
+            paymentReceivedDate: '2016-04-02',
+            shippingDate: '2016-04-03',
+            deliveryDate: null,
+            currentStatus: 'shipped'
         },
         {
-            name: 'Thinkpad',
-            manufacturer: 'Lenovo',
-            msrp: 800.00,
-            price: 1099.99,
-            description: 'ergonomic no-frills office productivity device',
-            imageUrls: ['http://www.lenovo.com/shop/americas/content/img_lib/subseries/T460-hero.png'
-            ],
-            type: 'base'
-        },
-        {
-            name: 'Spectre',
-            manufacturer: 'HP',
-            msrp: 1100.00,
-            price: 1300.99,
-            description: 'Someone call Guinness! HP’s new luxurious Spectre 13.3 ultraportable could now be the thinnest laptop in the world.',
-            imageUrls: ['http://core2.staticworld.net/images/article/2016/04/hp_spectre_13.3-100654198-large.jpg'
-            ],
-            type: 'base'
-        },
+            paymentReceivedDate: '2016-04-02',
+            shippingDate: null,
+            deliveryDate: null,
+            currentStatus: 'processing'
+        }
     ];
 
-    var creatingProducts = products.map(function (productObj) {
-        return Product.create(productObj);
+    var creatingOrders = orders.map(function (orderObj) {
+        return Order.create(orderObj);
     });
 
-    return Promise.all(creatingProducts);
+    return Promise.all(creatingOrders);
 
 };
 
 db.sync({ force: true })
     .then(function () {
-        return seedProducts();
+        return seedOrders();
     })
     .then(function () {
         console.log(chalk.green('Seed successful!'));
