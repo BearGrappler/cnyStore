a0b3b3a3cc3f4d05131fe9b4efeb53ec3b5c6bbd
Chris LoCasto <locasto1@gmail.com>
2016-09-13 11:50:16 -0500

    added component products to seed file

--- seedProducts.js
+++ seedProducts.js
@@ -65,6 +65,229 @@ var seedProducts = function () {
             ],
             type: 'base'
         },
+
+        // RAM Components
+        {
+            name: 'Vengeance 4GB (2 x 2GB)',
+            manufacturer: 'Corsair',
+            msrp: 22.99,
+            price: 18.99,
+            description: `DDR3 1600 (PC3 12800),
+                Timing 9-9-9-24,
+                Cas Latency 9,
+                Voltage 1.5V`,
+            imageUrls: ['http://pisces.bbystatic.com/image2/BestBuy_US/images/products/3027/3027187_sa.jpg;maxHeight=1000;maxWidth=1000'],
+            type: 'ram'
+        },
+        {
+            name: 'Vengeance 4GB (1 x 4GB)',
+            manufacturer: 'Corsair',
+            msrp: 24.99,
+            price: 21.99,
+            description: `DDR3 1600 (PC3 12800),
+                Timing 9-9-9-24,
+                Cas Latency 9,
+                Voltage 1.5V`,
+            imageUrls: ['http://pisces.bbystatic.com/image2/BestBuy_US/images/products/3027/3027187_sa.jpg;maxHeight=1000;maxWidth=1000'],
+            type: 'ram'
+        },
+        {
+            name: 'Vengeance 8GB (2 x 4GB)',
+            manufacturer: 'Corsair',
+            msrp: 42.99,
+            price: 38.99,
+            description: `DDR3 1600 (PC3 12800),
+                Timing 9-9-9-24,
+                Cas Latency 9,
+                Voltage 1.5V`,
+            imageUrls: ['http://pisces.bbystatic.com/image2/BestBuy_US/images/products/3027/3027187_sa.jpg;maxHeight=1000;maxWidth=1000'],
+            type: 'ram'
+        },
+        {
+            name: 'Vengeance 8GB (2 x 4GB)',
+            manufacturer: 'Corsair',
+            msrp: 48.99,
+            price: 46.99,
+            description: `DDR3 1600 (PC3 12800),
+                Timing 9-9-9-24,
+                Cas Latency 9,
+                Voltage 1.5V`,
+            imageUrls: ['http://pisces.bbystatic.com/image2/BestBuy_US/images/products/3027/3027187_sa.jpg;maxHeight=1000;maxWidth=1000'],
+            type: 'ram'
+        },
+        {
+            name: 'Vengeance 16GB (4 x 4GB)',
+            manufacturer: 'Corsair',
+            msrp: 68.99,
+            price: 64.99,
+            description: `DDR3 1600 (PC3 12800),
+                Timing 9-9-9-24,
+                Cas Latency 9,
+                Voltage 1.5V`,
+            imageUrls: ['http://pisces.bbystatic.com/image2/BestBuy_US/images/products/3027/3027187_sa.jpg;maxHeight=1000;maxWidth=1000'],
+            type: 'ram'
+        },
+        {
+            name: 'Vengeance 16GB (2 x 8GB)',
+            manufacturer: 'Corsair',
+            msrp: 88.99,
+            price: 86.99,
+            description: `DDR3 1600 (PC3 12800),
+                Timing 9-9-9-24,
+                Cas Latency 9,
+                Voltage 1.5V`,
+            imageUrls: ['http://pisces.bbystatic.com/image2/BestBuy_US/images/products/3027/3027187_sa.jpg;maxHeight=1000;maxWidth=1000'],
+            type: 'ram'
+        },
+
+        // CPU components
+        {
+            name: 'Core i3',
+            manufacturer: 'Intel',
+            msrp: 249.99,
+            price: 234.99,
+            description: `LGA 1151,
+                Unlocked Processor, 
+                DDR4 & DDR3L Support, 
+                Display Resolution up to 4096 x 2304, 
+                Intel Turbo Boost Technology, 
+                Compatible with Intel 100 Series Chipset Motherboards`,
+            imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+            type: 'cpu'
+        },
+        {
+            name: 'Core i5',
+            manufacturer: 'Intel',
+            msrp: 249.99,
+            price: 234.99,
+            description: `LGA 1151,
+                Unlocked Processor, 
+                DDR4 & DDR3L Support, 
+                Display Resolution up to 4096 x 2304, 
+                Intel Turbo Boost Technology, 
+                Compatible with Intel 100 Series Chipset Motherboards`,
+            imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+            type: 'cpu'
+        },
+        {
+            name: 'Core i7',
+            manufacturer: 'Intel',
+            msrp: 349.99,
+            price: 334.99,
+            description: `LGA 1151,
+                Unlocked Processor, 
+                DDR4 & DDR3L Support, 
+                Display Resolution up to 4096 x 2304, 
+                Intel Turbo Boost Technology, 
+                Compatible with Intel 100 Series Chipset Motherboards`,
+            imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+            type: 'cpu'
+        },
+        {
+            name: 'Core i9',
+            manufacturer: 'Intel',
+            msrp: 449.99,
+            price: 434.99,
+            description: `LGA 1151,
+                Unlocked Processor, 
+                DDR4 & DDR3L Support, 
+                Display Resolution up to 4096 x 2304, 
+                Intel Turbo Boost Technology, 
+                Compatible with Intel 100 Series Chipset Motherboards`,
+            imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+            type: 'cpu'
+        },
+        {
+            name: 'Core i11',
+            manufacturer: 'Intel',
+            msrp: 449.99,
+            price: 434.99,
+            description: `LGA 1151,
+                Unlocked Processor, 
+                DDR4 & DDR3L Support, 
+                Display Resolution up to 4096 x 2304, 
+                Intel Turbo Boost Technology, 
+                Compatible with Intel 100 Series Chipset Motherboards`,
+            imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+            type: 'cpu'
+        },
+
+        // HDD components
+        {
+            name: '850 EVO 250GB',
+            manufacturer: 'Samsung',
+            msrp: 169.99,
+            price: 157.99,
+            description: "Achieve the ultimate read/write performance to maximize your everyday computing experience with Samsung's TurboWrite technology. You only obtain a 10% better user.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRSGrFH-UGa1tkzURSkIvRms_QOOkbY3-dfTMgeW4OP4DfmxEZuahnqbpAIF40lqCR3mJ4jmos&usqp=CAE'],
+            type: 'hdd'
+        },
+        {
+            name: '850 EVO 500GB',
+            manufacturer: 'Samsung',
+            msrp: 269.99,
+            price: 257.99,
+            description: "Achieve the ultimate read/write performance to maximize your everyday computing experience with Samsung's TurboWrite technology. You only obtain a 10% better user.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRSGrFH-UGa1tkzURSkIvRms_QOOkbY3-dfTMgeW4OP4DfmxEZuahnqbpAIF40lqCR3mJ4jmos&usqp=CAE'],
+            type: 'hdd'
+        },
+        {
+            name: '850 EVO 750GB',
+            manufacturer: 'Samsung',
+            msrp: 369.99,
+            price: 357.99,
+            description: "Achieve the ultimate read/write performance to maximize your everyday computing experience with Samsung's TurboWrite technology. You only obtain a 10% better user.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRSGrFH-UGa1tkzURSkIvRms_QOOkbY3-dfTMgeW4OP4DfmxEZuahnqbpAIF40lqCR3mJ4jmos&usqp=CAE'],
+            type: 'hdd'
+        },
+        {
+            name: '850 EVO 1TB',
+            manufacturer: 'Samsung',
+            msrp: 469.99,
+            price: 457.99,
+            description: "Achieve the ultimate read/write performance to maximize your everyday computing experience with Samsung's TurboWrite technology. You only obtain a 10% better user.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRSGrFH-UGa1tkzURSkIvRms_QOOkbY3-dfTMgeW4OP4DfmxEZuahnqbpAIF40lqCR3mJ4jmos&usqp=CAE'],
+            type: 'hdd'
+        },
+
+        // GPU components
+        {
+            name: 'GeForce GTX 730 Graphics Card',
+            manufacturer: 'EVGA',
+            msrp: 69.99,
+            price: 57.99,
+            description: "Speed up your PC experience when you upgrade from integrated graphics to the NVIDIA GeForce GT 730 dedicated card.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSAnfjGLF0RmAE7j8dE64AD7LJMeI5f8pX43UCIuPuE2KugBQ8&usqp=CAE'],
+            type: 'gpu'
+        },
+        {
+            name: 'GeForce GTX 980 Graphics Card',
+            manufacturer: 'EVGA',
+            msrp: 369.99,
+            price: 357.99,
+            description: "Speed up your PC experience when you upgrade from integrated graphics to the NVIDIA GeForce GT 730 dedicated card.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSAnfjGLF0RmAE7j8dE64AD7LJMeI5f8pX43UCIuPuE2KugBQ8&usqp=CAE'],
+            type: 'gpu'
+        },
+        {
+            name: 'GeForce GTX 960 Graphics Card',
+            manufacturer: 'EVGA',
+            msrp: 269.99,
+            price: 257.99,
+            description: "Speed up your PC experience when you upgrade from integrated graphics to the NVIDIA GeForce GT 730 dedicated card.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSAnfjGLF0RmAE7j8dE64AD7LJMeI5f8pX43UCIuPuE2KugBQ8&usqp=CAE'],
+            type: 'gpu'
+        },
+        {
+            name: 'GeForce GTX 970 Graphics Card',
+            manufacturer: 'EVGA',
+            msrp: 169.99,
+            price: 157.99,
+            description: "Speed up your PC experience when you upgrade from integrated graphics to the NVIDIA GeForce GT 730 dedicated card.",
+            imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSAnfjGLF0RmAE7j8dE64AD7LJMeI5f8pX43UCIuPuE2KugBQ8&usqp=CAE'],
+            type: 'gpu'
+        }
+
     ];
 
     var creatingProducts = products.map(function (productObj) {
