fda8c832f11c609a4ed285d70a5b68d1ab2e5918
Chris LoCasto <locasto1@gmail.com>
2016-09-13 11:07:59 -0500

    Adjusted review validation limits & refactored to require user account for a review

--- server/db/models/reviews.js
+++ server/db/models/reviews.js
@@ -1,17 +1,13 @@
+'use strict';
 var Sequelize = require('sequelize');
-
 var db = require('../_db');
 
 module.exports = db.define('review', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
     writtenReview: {
-        type: Sequelize.STRING(500),
+        type: Sequelize.STRING(3000),
         allowNull: false,
         validate:{
-            len: [20, 500]  //to satisfy validation requirement that the review has to have a minimum length
+            len: [20, 3000]  //to satisfy validation requirement that the review has to have a minimum length
         }
     },
     rating: {
@@ -19,8 +15,7 @@ module.exports = db.define('review', {
         allowNull: false,
         validate: {
             max: 5,
-            min: 0
+            min: 1
         }
     }
-}
-);
+});
