eb9881484401431ceb5a00acd9cb370fbd71a6ce
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-14 11:52:35 -0500

    changed the associations between products/options to allow for multiple foreign keys

--- server/db/index.js
+++ server/db/index.js
@@ -29,9 +29,10 @@ Product.Review = Product.hasMany(Review, { as: 'Reviews' });
 Product.Order = Product.belongsToMany(Order, { through: 'OrderProduct', as: 'LineItems' });
 Order.Product = Order.belongsToMany(Product, { through: 'OrderProduct', as: 'Orders' });
 
-Option.Base = Option.hasMany(Product, { as: 'BaseModels', foreignKey: 'baseId' });
-Option.Upgrade = Option.hasMany(Product, { as: 'Upgrades', foreignKey: 'upgradeId' });
+Option.Base = Option.belongsTo(Product, { as: 'BaseModels', foreignKey: 'baseId' });
+Option.Upgrade = Option.belongsTo(Product, { as: 'Upgrades', foreignKey: 'upgradeId' });
 
 Address.Order = Address.hasMany(Order, {as: 'Purchases'});
 
 module.exports = db;
+
