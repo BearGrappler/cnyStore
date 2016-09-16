cdf8d4fdd63384e9f0e4ae2f95deb44d884b25f6
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-15 23:29:11 -0500

    all routes functioning

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -3,36 +3,38 @@ let Product = require('../../../db').model('Product')
 let Option = require('../../../db').model('Option')
 
 router.get('/:id', function(req, res, next) {
-  Product.findOne({where: {id: req.params.id}})
-  .then(product => res.send(product))
+  Product.findOne({ where: { id: req.params.id } })
+    .then(product => res.send(product))
 });
 
 router.get('/:id/upgrades', function(req, res, next) {
-  Option.findAll({where: {baseId: req.params.id}, include: {model: Product, as: 'Upgrades'}})
-  .then(options => {
-    options.forEach(option => {
-      if (option.defOption) {
-        option.Upgrades.dataValues.defOption = true;
-        console.log(option.Upgrades)
-      }
+  Option.findAll({ where: { baseId: req.params.id }, include: { model: Product, as: 'Upgrades' } })
+    .then(options => {
+      options.forEach(option => {
+        if (option.defOption) {
+          option.Upgrades.dataValues.defOption = true;
+        }
+      })
+      return options;
     })
-    return options;
-  })
-  .then(options => res.send(options));
+    .then(options => res.send(options));
 });
 
 router.put('/:id', function(req, res, next) {
-  //need admin authentication check
   //NEED TO SANITIZE
-  Product.findOne({where: {id: req.params.id}})
-  .then(product => product.update(req.body))
-  .then(product => res.send(product))
+  if (req.user && req.user.isAdmin) {
+    Product.findOne({ where: { id: req.params.id } })
+      .then(product => product.update(req.body))
+      .then(product => res.send(product))
+  }
 });
 
 router.delete('/:id', function(req, res, next) {
-  Product.findOne({where: {id: req.params.id}})
-  .then(product => product.destroy())
-  .then(() => res.sendStatus(204))
+  if (req.user && req.user.isAdmin) {
+    Product.findOne({ where: { id: req.params.id } })
+      .then(product => product.destroy())
+      .then(() => res.sendStatus(204))
+  }
 });
 
 module.exports = router;
