e18c77ffb2534e6281810027d2adcd5bd8e399f7
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-16 13:49:56 -0500

    Update _db.js

--- server/db/_db.js
+++ server/db/_db.js
@@ -2,9 +2,11 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
-var db = new Sequelize(env.DATABASE_URI, {
+var db = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD {
   logging: env.LOGGING,
-  native: env.NATIVE
+  native: env.NATIVE,
+  dialect: env.DATABASE_DIALECT,
+  port: env.DATABASE_PORT
 });
 
 module.exports = db;
