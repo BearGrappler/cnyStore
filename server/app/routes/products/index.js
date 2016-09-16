9ab8696563f6ff19da3445d4244bb7b0a89878de
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-16 13:44:56 -0500

    Update index.js

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -1,3 +1,4 @@
+'use strict'
 let router = require('express').Router(); // eslint-disable-line new-cap
 let Product = require('../../../db').model('Product')
 let Option = require('../../../db').model('Option')
