400eb1a84293601d78ffdc39133f9c23aeceff80
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-19 16:59:26 -0500

    fixed review seed data and made reviews visible on single-product page

--- seed/seed.js
+++ seed/seed.js
@@ -23,67 +23,78 @@ const Promise = require('sequelize').Promise;
 
 let seedUsers = function() {
 
-    let users = require('./seedUsers.js')
+  let users = require('./seedUsers.js')
 
-    let creatingUsers = users.map(userObj => db.model('User').create(userObj))
+  let creatingUsers = users.map(userObj => db.model('User').create(userObj))
 
-    return Promise.all(creatingUsers);
+  return Promise.all(creatingUsers);
 };
 
 let seedOrders = function() {
-    let orders = require('./seedOrders');
+  let orders = require('./seedOrders');
 
-    let creatingOrders = orders.map(orderObj => db.model('Order').create(orderObj))
+  let creatingOrders = orders.map(orderObj => db.model('Order').create(orderObj))
 
-    return Promise.all(creatingOrders)
+  return Promise.all(creatingOrders)
 }
 
 let seedProducts = function() {
-    let products = require('./seedProducts');
+  let products = require('./seedProducts');
 
-    let creatingProducts = products.map(productObj => db.model('Product').create(productObj))
+  let creatingProducts = products.map(productObj => db.model('Product').create(productObj))
 
-    return Promise.all(creatingProducts)
+  return Promise.all(creatingProducts)
 }
 
 let seedAddresses = function() {
-    let addresses = require('./seedAddresses');
+  let addresses = require('./seedAddresses');
 
-    let creatingAddresses = addresses.map(addressObj => db.model('Address').create(addressObj))
+  let creatingAddresses = addresses.map(addressObj => db.model('Address').create(addressObj))
 
-    return Promise.all(creatingAddresses)
+  return Promise.all(creatingAddresses)
 }
 
 let seedOptions = function() {
-    let options = require('./seedOptions');
+  let options = require('./seedOptions');
 
-    let creatingOptions = options.map(optionObj => db.model('Option').create(optionObj))
+  let creatingOptions = options.map(optionObj => db.model('Option').create(optionObj))
 
-    return Promise.all(creatingOptions)
+  return Promise.all(creatingOptions)
+}
+
+let seedReviews = function() {
+  let reviews = require('./seedReviews');
+
+  let creatingReviews = reviews.map(reviewObj => db.model('Review').create(reviewObj))
+
+  return Promise.all(creatingReviews)
 }
 
 module.exports = (function() {
-    return db.sync({ force: true })
-        .then(function() {
-            return seedUsers();
-        })
-        .then(() => {
-            return seedOrders();
-        })
-        .then(() => {
-            return seedProducts();
-        })
-        .then(() => {
-            return seedAddresses();
-        })
-        .then(() => {
-            return seedOptions();
-        })
-        .then(function() {
-            return console.log(chalk.green('Seed successful!'));
-        })
-        .catch(function(err) {
-            console.error(err);
-            process.exit(1);
-        });
+  return db.sync({ force: true })
+    .then(function() {
+      return seedUsers();
+    })
+    .then(() => {
+      return seedOrders();
+    })
+    .then(() => {
+      return seedProducts();
+    })
+    .then(() => {
+      return seedAddresses();
+    })
+    .then(() => {
+      return seedOptions();
+    })
+    .then(() => {
+      return seedReviews();
+    })
+    .then(function() {
+      return console.log(chalk.green('Seed successful!'));
+    })
+    .catch(function(err) {
+      console.error(err);
+      process.exit(1);
+    });
 }());
