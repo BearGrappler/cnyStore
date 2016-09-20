d7eef82c4361385e1995d73767ca1e9ac717b24c
Chris <locasto1@gmail.com>
2016-09-19 19:25:17 -0500

    Update admins-factory.js
    Removing console.logs

--- browser/js/admins-only/admins-factory.js
+++ browser/js/admins-only/admins-factory.js
@@ -3,22 +3,16 @@ app.factory('adminsFactory', function($http) {
 
 
     adminsFactory.makeUserAdmin = function(user){
-        console.log('Hit makeUserAdmin func in adminsFactory' , user);
-
         return $http.put('/api/users/makeAdmin', user)
-
     }
 
     adminsFactory.deleteUser = function(user){
-        console.log('Hit deleteUser func in adminsFactory' , user);
         return $http.delete('/api/users/deleteUser/' + user.id)
 
     }
 
     adminsFactory.changeStatusOfOrder = function(order, newStatus){
-        console.log('Hit makeUserAdmin func in adminsFactory' , order);
         return $http.put('/api/orders/adminsOnly/' + order.id +'/'+ newStatus)
-
     }
     return adminsFactory;
 });
