8d132afafd1002b22b19a65e78a620eea863e546
nrod80 <nicholas.rodriguez@duke.edu>
2016-09-21 02:33:54 -0500

    updated seed data

--- seed/seedProducts.js
+++ seed/seedProducts.js
@@ -1,19 +1,30 @@
-module.exports = [{
-    name: 'Macbook Silver',
-    manufacturer: 'Apple',
-    msrp: 800.00,
-    price: 999.99,
-    description: 'stylish laptop for college students',
-    imageUrls: ['http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/air/macbook-air-13-select-hero-201505?wid=265&hei=154&fmt=png-alpha&qlt=95&.v=YGXjP3', 'http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbookair/select/macbookair-select-box-201504?wid=553&hei=399&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=HWal10'],
+module.exports = [
+
+  {
+    name: 'LG Gram',
+    manufacturer: 'LG',
+    msrp: '949.99',
+    price: '949.99',
+    description: 'Take the lightweight LG 15.6 gram 15 Notebook with you while on the go. Weighing less than 2.2 pounds and measuring just 0.7 thin the LG gram 15 is made of magnesium alloy and has a gold finish. The 15.6 Full HD IPS display features a 1920 x 1080 screen resolution a 16:9 aspect ratio and wider viewing angles. You can also output video to an external display via HDMI. The system is powered by a 6th Gen Skylake Intel Core i5-6200U dual-core processor. It also has 8GB of 1600 MHz DDR3L RAM a 256GB solid state drive a microSD media card slot 802.11ac Wi-Fi Bluetooth 4.0 LE a USB 3.0 Type-C port a 720p webcam an integrated microphone and built-in stereo speakers. Windows 10 Home 64-bit is the installed operating system.',
+    imageUrls: ['https://sellbroke.com/wp-content/uploads/2016/02/LG_Gram_15-4.png', 'http://edge.alluremedia.com.au/m/g/2014/09/lg-ultra-pc-1.jpg'],
     type: 'base',
     userType: 'student'
   }, {
-    name: 'Alienware Gaming Laptop',
+    name: 'MacBook Air',
+    manufacturer: 'Apple',
+    msrp: 800.00,
+    price: 999.99,
+    description: 'Originally released March 2015; 13.3-inch (diagonal) LED-backlit glossy widescreen display with support for millions of colors',
+    imageUrls: ['http://www.iclarified.com/images/news/46186/212581/212581.png', 'https://upload.wikimedia.org/wikipedia/commons/d/d5/MacBook_Air_Mid_2012.png'],
+    type: 'base',
+    userType: 'student'
+  }, {
+    name: 'Alienware',
     manufacturer: 'Dell',
     msrp: 900.00,
     price: 1199.99,
-    description: 'gaming device for intense mountain-dew guzzlers',
-    imageUrls: ['http://scene7-cdn.dell.com/is/image/DellComputer/laptop-alienware-15-r2-polaris-mixed-set-video?hei=400&wid=965', 'http://shop-media.intel.com/api/v2/helperservice/getimage?url=http://images.icecat.biz/img/gallery/29798850_5705.jpg&height=550&width=550'],
+    description: 'The durable anodized aluminum shell, reinforced by a magnesium-alloy frame, protects your LCD and the internal components. Angled front and side walls give the Alienware 15 an elite and stealth look. And it’s designed for maximum performance, with a rear GPU/CPU exhaust vent to manage heat generated inside the system.',
+    imageUrls: ['http://image.alienware.com/images/landings/laptops/alienware-18-fx-green-500x500.png', 'http://i.dell.com/das/xa.ashx/global-site-design%20WEB/81428488-a15d-f2a0-708c-facbd5e6cfd2/1/OriginalPng?id=Dell/Product_Images/Dell_Client_Products/Notebooks/Alienware_Notebooks/alienware_m17x/hero/laptop-alienware-m17x-r4-red-left-hero-504x350.png'],
     type: 'base',
     userType: 'gamer'
   }, {
@@ -21,21 +32,57 @@ module.exports = [{
     manufacturer: 'Lenovo',
     msrp: 800.00,
     price: 1099.99,
-    description: 'ergonomic no-frills office productivity device',
-    imageUrls: ['http://www.lenovo.com/shop/americas/content/img_lib/subseries/T460-hero.png'],
+    description: 'Give your small business a unique style with the compelling facade and features of these business laptops. They\'re built for security and productivity, with a look that makes business personal again The 15.6 " display features a screen resolution of 1366 x 768, an anti-glare coating, LED-backlighting, a brightness of 220 cd/m2, and a 16:9 aspect ratio.',
+    imageUrls: ['http://www.lenovo.com/shop/americas/content/img_lib/subseries/T460-hero.png', 'https://www.vernoncomputersource.com/wp-content/uploads/E420.png'],
     type: 'base',
     userType: 'casual'
   }, {
-    name: 'Spectre',
+    name: 'Spectre 13',
     manufacturer: 'HP',
     msrp: 1100.00,
     price: 1300.99,
     description: 'Someone call Guinness! HP’s new luxurious Spectre 13.3 ultraportable could now be the thinnest laptop in the world.',
-    imageUrls: ['http://core2.staticworld.net/images/article/2016/04/hp_spectre_13.3-100654198-large.jpg'],
+    imageUrls: ['https://h20386.www2.hp.com/CanadaStore/html/Merch/campaigns/hp-spectre/laptop_56ec2095ec0cd_1.png', 'http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05059983.png'],
     type: 'base',
     userType: 'artist'
-  },
+  }, {
+    name: 'MacBook Pro',
+    manufacturer: 'Apple',
+    msrp: 1099,
+    price: 1099,
+    description: 'With fifth-generation Intel Core processors, the latest graphics, and faster flash storage, the incredibly advanced MacBook Pro with Retina display moves even further ahead in performance and battery life.',
+    imageUrls: ['http://orig02.deviantart.net/dc78/f/2013/312/9/7/os_x_mavericks__macbook_png__by_sucxces-d6t59mq.png', 'http://4.bp.blogspot.com/-YwN-M8EYt5E/UIlTvqikSMI/AAAAAAAAcLs/x8fVvsa_evg/s1600/macbook+pro+retina+display-2012.png'],
+    type: 'base',
+    userType: 'artist'
+  }, {
+    name: 'XPS 13',
+    manufacturer: 'Dell',
+    price: 1149,
+    msrp: 1149,
+    description: 'The smallest 13-inch laptop on the planet has the world’s first virtually borderless InfinityEdge display — amazing both inside and out.',
+    imageUrls: ['http://dri2.img.digitalrivercontent.net/Storefront/Company/msintl/images/English/en-INTL-Dell-XPS-13-9343-6364SLV-i5-256GB-Silver-Androidized-CWF-01966/PDP/en-INTL-PDP0-Dell-XPS-13-9343-6364SLV-i5-256GB-Silver-Androidized-CWF-01966-Large-desktop.png', 'http://www.windowscentral.com/sites/wpcentral.com/files/styles/large/public/topic_images/2015/Dell-XPS-13.png?itok=CmizNNn2'],
+    userType: 'student',
+    type: 'base'
+  }, {
+    name: 'Spectre x360',
+    manufacturer: 'HP',
+    msrp: 1250,
+    price: 1250,
+    description: 'Cover every angle with the ultra-slim and flexible HP Spectre x360. This convertible PC can be used four ways—laptop, stand, tent, and tablet—thanks to a unique 360-degree hinge that flips to the perfect view.',
+    imageUrls: ['http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c04840944.png', 'http://store.hp.com/us/en/pdp/hp-spectre-x360-convertible-laptop---15t-p-t6t12ua-aba'],
+    type: 'base',
+    userType: 'artist'
 
+  }, {
+    name: 'Razer Blade',
+    manufacturer: 'Razer',
+    msrp: 1499,
+    price: 1499,
+    description: 'The state-of-the-art IGZO (Indium Gallium Zinc Oxide) display panel technology in the Razer Blade provides you with unmatched color and image quality. As IGZO has a significantly faster propagation time than traditional a-Si TFT displays, video response is improved, resulting in consistent colors across the whole screen, so you get impressively smooth motion video and immersive gameplay every time.',
+    imageUrls: ['http://assets.razerzone.com/eeimages/razer_pages/16039/razer-blade-gallery-v3-04.png', 'http://assets.razerzone.com/eeimages/products/13148/blade-14inch-gallery-3-v6.png'],
+    type: 'base',
+    userType: 'gamer'
+  },
   // RAM Components
   {
     name: 'Vengeance 4GB (2 x 2GB)',
@@ -107,6 +154,19 @@ module.exports = [{
 
   // CPU components
   {
+    name: 'Core M',
+    manufacturer: 'Intel',
+    msrp: 149.99,
+    price: 134.99,
+    description: `LGA 1151,
+            Unlocked Processor,
+            DDR4 & DDR3L Support,
+            Display Resolution up to 4096 x 2304,
+            Intel Turbo Boost Technology,
+            Compatible with Intel 100 Series Chipset Motherboards`,
+    imageUrls: ['http://vignette3.wikia.nocookie.net/logopedia/images/0/05/Core_M.png/revision/latest?cb=20151015133043'],
+    type: 'cpu'
+  }, {
     name: 'Core i3',
     manufacturer: 'Intel',
     msrp: 249.99,
@@ -117,23 +177,10 @@ module.exports = [{
             Display Resolution up to 4096 x 2304,
             Intel Turbo Boost Technology,
             Compatible with Intel 100 Series Chipset Motherboards`,
-    imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+    imageUrls: ['http://vignette1.wikia.nocookie.net/logopedia/images/9/94/Intel_Core_i3.png/revision/latest?cb=20120712002147'],
     type: 'cpu'
   }, {
     name: 'Core i5',
-    manufacturer: 'Intel',
-    msrp: 249.99,
-    price: 234.99,
-    description: `LGA 1151,
-            Unlocked Processor,
-            DDR4 & DDR3L Support,
-            Display Resolution up to 4096 x 2304,
-            Intel Turbo Boost Technology,
-            Compatible with Intel 100 Series Chipset Motherboards`,
-    imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
-    type: 'cpu'
-  }, {
-    name: 'Core i7',
     manufacturer: 'Intel',
     msrp: 349.99,
     price: 334.99,
@@ -143,10 +190,10 @@ module.exports = [{
             Display Resolution up to 4096 x 2304,
             Intel Turbo Boost Technology,
             Compatible with Intel 100 Series Chipset Motherboards`,
-    imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+    imageUrls: ['http://vignette1.wikia.nocookie.net/logopedia/images/1/1a/Core_i5.PNG/revision/latest?cb=20140207102648'],
     type: 'cpu'
   }, {
-    name: 'Core i9',
+    name: 'Core i7',
     manufacturer: 'Intel',
     msrp: 449.99,
     price: 434.99,
@@ -156,20 +203,7 @@ module.exports = [{
             Display Resolution up to 4096 x 2304,
             Intel Turbo Boost Technology,
             Compatible with Intel 100 Series Chipset Motherboards`,
-    imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
-    type: 'cpu'
-  }, {
-    name: 'Core i11',
-    manufacturer: 'Intel',
-    msrp: 449.99,
-    price: 434.99,
-    description: `LGA 1151,
-            Unlocked Processor,
-            DDR4 & DDR3L Support,
-            Display Resolution up to 4096 x 2304,
-            Intel Turbo Boost Technology,
-            Compatible with Intel 100 Series Chipset Motherboards`,
-    imageUrls: ['http://images17.newegg.com/is/image/newegg/19-117-561-TS?$S640$'],
+    imageUrls: ['http://vignette1.wikia.nocookie.net/logopedia/images/6/61/Core_i7.PNG/revision/latest?cb=20140207112851'],
     type: 'cpu'
   },
 
@@ -210,7 +244,7 @@ module.exports = [{
 
   // GPU components
   {
-    name: 'GeForce GTX 730 Graphics Card',
+    name: 'GeForce GTX 950 Graphics Card',
     manufacturer: 'EVGA',
     msrp: 69.99,
     price: 57.99,
@@ -228,16 +262,16 @@ module.exports = [{
   }, {
     name: 'GeForce GTX 960 Graphics Card',
     manufacturer: 'EVGA',
-    msrp: 269.99,
-    price: 257.99,
+    msrp: 169.99,
+    price: 157.99,
     description: "Speed up your PC experience when you upgrade from integrated graphics to the NVIDIA GeForce GT 730 dedicated card.",
     imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSAnfjGLF0RmAE7j8dE64AD7LJMeI5f8pX43UCIuPuE2KugBQ8&usqp=CAE'],
     type: 'gpu'
   }, {
     name: 'GeForce GTX 970 Graphics Card',
     manufacturer: 'EVGA',
-    msrp: 169.99,
-    price: 157.99,
+    msrp: 269.99,
+    price: 257.99,
     description: "Speed up your PC experience when you upgrade from integrated graphics to the NVIDIA GeForce GT 730 dedicated card.",
     imageUrls: ['https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSAnfjGLF0RmAE7j8dE64AD7LJMeI5f8pX43UCIuPuE2KugBQ8&usqp=CAE'],
     type: 'gpu'
